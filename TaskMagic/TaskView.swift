00f6676b4a1757d4250ac2bf3d462515a149a00b
Collin Argo <collinargo@gmail.com>
2018-09-23 11:45:00 -0400

    fix deselect rows after editing

--- TaskMagic/TaskView.swift
+++ TaskMagic/TaskView.swift
@@ -177,7 +177,7 @@ class TaskView : UIViewController, UINavigationControllerDelegate, UITableViewDe
             searchBar.text = ""
             searchBar.showsCancelButton = false
             searchBar.resignFirstResponder()
-            
+            tableView.deselectRow(at: tableView.indexPathForSelectedRow!, animated: false)
             parentTask.clearSelections()
         }
         navigationController?.setToolbarHidden(!isEditing, animated: true)
